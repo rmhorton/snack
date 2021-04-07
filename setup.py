fd509c730eaee746794625e6ac8e5a6767827451
Robert M. Horton, PhD <rhorton@microsoft.com>
2021-04-07 01:19:34 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+# python setup.py bdist_wheel
+# pip install --force-reinstall --user dist\snack-0.0.1-py3-none-any.whl
+
+import io
+from setuptools import setup, find_packages
+
+# import snack
+
+with io.open('README.md', encoding='utf_8') as fp:
+    readme = fp.read()
+
+setup(
+    name='snack',
+    version='0.0.1', # snack.__version__,
+    description='feature engineering functions for EHR data', # snack.__doc__.strip(),
+    author= 'Robert Horton',
+    url='https://github.com/rmhorton/snack',
+    long_description=readme,
+    long_description_content_type='text/markdown; charset=UTF-8',
+    packages=find_packages(),
+    install_requires = ['networkx>=2.5', 'python-louvain>=0.15']
+)
+
